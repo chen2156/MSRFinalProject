4429e62a38e353cee4b90578fc3a07f2b6e7e013
Jason <jasonchen2021@u.northwestern.edu>
2021-11-23 11:54:58 -0600

    Update PCA code to work with libraries

--- laser_values/src/PCA.py
+++ laser_values/src/PCA.py
@@ -34,48 +34,43 @@ def PCAImageCompression(imageFile):
 
     return imageCompressed
 
-def PCAImagesCompression(fileDirectory):
+def PCAImagesCompression(fileDirectory, numImages):
     
     listing = os.listdir(fileDirectory)
-    numImages = 1
     randomIndexes = random.sample(range(0, len(listing)), numImages)
-    print(randomIndexes)
+
 
     flattenedMatrix = np.empty((657, 360))
     for i in range(numImages):
         image = cv2.imread(fileDirectory + listing[randomIndexes[i]])
-        print(image.shape)
         resized = cv2.resize(image, (360, 219), interpolation=cv2.INTER_LINEAR)
         resized = cv2.cvtColor(resized, cv2.COLOR_BGR2HSV)
-        print(resized.shape)
-        print(resized.size)
-
         H, S, V = cv2.split(resized)
-
-
         scaledH = H / 180
         scaledS = S / 255
         scaledV = V / 255
-        
         flattenedImage = np.vstack((scaledH, scaledS, scaledV))
         if i == 0:
             flattenedMatrix[:] = flattenedImage
         else:
             flattenedMatrix = np.hstack((flattenedMatrix, flattenedImage))    
 
-
     finalMatrix = np.empty((6, 1))
+    pca = PCA(6)
 
-    pca6 = PCA(n_components=6)
+    pca.fit(flattenedMatrix.T)
+
+    finalMatrix = pca.transform(flattenedMatrix.T)
+
+
+    '''
     for i in range(flattenedMatrix.shape[1]):
-        if i == 10:
+        if i == 100:
             break    
         reshap = flattenedMatrix[:, i].reshape(-1, 1)
-        print("reshape's shape is")
-        print(reshap.shape)
+     
         covMatrix = np.cov(reshap)
-        print("Covariance Matrix shape is")
-        print(covMatrix.shape)
+        
         covMatrix = np.nan_to_num(covMatrix)
         eigenValues, eigenVectors = np.linalg.eig(covMatrix)
 
@@ -85,107 +80,41 @@ def PCAImagesCompression(fileDirectory):
 
         featureVector = eigenVectors[:, len(eigenVectors) - 6: len(eigenVectors)]
 
-        print("shape of featureVector is")
-        print(featureVector.shape)
+   
 
         finalTransform = np.matmul(featureVector.T, reshap)
 
-        print("shape of finalTransform is")
-        print(finalTransform.shape)
-
+       
 
         if i == 0:
             finalMatrix[:] = finalTransform
-            print("shape of finalMatrix is")
-            print(finalMatrix.shape)
-
-            print(finalMatrix.shape)
+           
         else:
             finalMatrix = np.hstack((finalMatrix, finalTransform))
-            print("shape of finalMatrix is")
-            print(finalMatrix.shape)
-    print(finalMatrix.shape)
-
+    '''
     return finalMatrix
 
 
 
 if __name__ == "__main__":
-    print("Start the processs")
-    finalComponents = PCAImagesCompression("/home/chen2156/laserData/src/laser_values/src/multipleImages/unWarpedImages/")
-    print("Shape of final Component is")
-    print(finalComponents.shape)
-
+    #NumImages = TrainingSize + 1
+    numImages = 11
+    finalComponents = PCAImagesCompression("/home/chen2156/laserData/src/laser_values/src/multipleImages/unWarpedImages/", numImages)
     ObservedData = np.loadtxt("/home/chen2156/laserData/src/laser_values/src/multipleImages/laserDataCaputer.csv", delimiter=',')
-
-    x = np.atleast_2d(np.linspace(0, 360, 1000)).T
-
+    x = np.atleast_2d(np.linspace(0, 360, 100)).T
     kernel = RBF()
     gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=100)
+    y = np.tile(ObservedData, numImages - 1)
+    X = finalComponents[0:360 * (numImages - 1), :]
+    x = finalComponents[360 * (numImages - 1):360 * numImages, :]
+    gp.fit(X, y)
 
-    ypredResult = []
-    MSEResult = []
-    for c in range(finalComponents.shape[1]):
-
-        X = finalComponents[:, c].reshape(-1, 1)
-
-        print(X)
-        y = np.array([ObservedData[c % 360]]).reshape(1, -1)
-
-        print(y)
-
-        print("Shapes are")
-        print(X.shape)
-        print(y.shape)
-        gp.fit(X.T, y)
-        x = c % 360
-        y_pred, MSE = gp.predict(x, return_std=True)
-        ypredResult.append(y_pred)
-        MSEResult.append(MSE)
-
-    print("Results are")
-    print(ypredResult)
-    print(MSEResult)
-
-
-    #kernel = C(1.0, (1e-3, 1e3)) * RBF([5,5], (1e-2, 1e2))
-
-    #if max([varH, varS, varV]) == varH:
-    #    X = projH[0]
-    #elif max([varH, varS, varV]) == varS:
-    ##    X = projS[0]
-    #else:
-    #    X = projV[0]
-    #kernel = RBF(length_scale=[1./X.shape[0] for i in range(X.shape[0])])
-    #kernel = C(1.0, (1e-3, 1e3)) * RBF(10, (1e-2, 1e2))
-
-
-
-    # f = open("/home/chen2156/laserData/src/laser_values/src/multipleImages/laserDataCaputer.txt", "r")
-    
-    #line = f.readline()
-
-    #y  = line.split(",")
-    #y = np.array(list(map(float, y)))
-    #print("Dimensions are")
-
-    
-  
-
-    
-    #x = np.array(range(360)).reshape(1, -1)
-    #print("Shape is")
-    #print(x.shape)
-    
-    
- 
-    
+    y_pred, sigma = gp.predict(x, return_std=True)  
     plt.figure()
-    #plt.plot(x, f(x), "r:", label=r"$f(x) = x\,\sin(x)$")
-    plt.plot(X, y, "r.", markersize=10, label="Observations")
-    plt.plot(x, y_pred, "b-", label="Prediction")
+    plt.plot(range(360), ObservedData, "r.", markersize=10, label="Observations")
+    plt.plot(range(360), y_pred, "b-", label="Prediction")
     plt.fill(
-        np.concatenate([x, x[::-1]]),
+        np.concatenate([range(360), range(360)[::-1]]),
         np.concatenate([y_pred - 1.9600 * sigma, (y_pred + 1.9600 * sigma)[::-1]]),
         alpha=0.5,
         fc="b",
@@ -196,8 +125,7 @@ if __name__ == "__main__":
     plt.ylabel("Distance (m)")
     plt.ylim(-10, 20)
     plt.legend(loc="upper left")
-    plt.show()
-    
+    plt.savefig("/home/chen2156/laserData/src/laser_values/src/multipleImages/10TrainingImages.png")
     
     
 
